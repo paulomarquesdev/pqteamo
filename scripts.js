4b980135a0faf0d637464de26129ed0212da03d6
Paulo Marques <paulomarques.rs1@gmail.com>
2023-01-29 18:21:46 -0300

    logica

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var screen = document.querySelectorAll(".screen");
-var i = 0;
 
 function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
 async function main() {
+    var i = 0;
     while (i<4){
         var element = screen[i]
         element.style.display = "flex";
