e0016b1b6b0c47eb768f9acb7d3af025e911f316
Paulo Marques <paulomarques.rs1@gmail.com>
2023-01-29 18:13:48 -0300

    Logica

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-var body = document.querySelector("body");
-console.log(body[1])
+var screen = document.querySelectorAll(".screen");
+var i = 0;
 
-for(var i = 1; i <= 4; i++){
-    var element = body.querySelector(`screen${i}`)
-    setTimeout(() => {
-        element.classList.add("enabled")
-    }, 2000)
-    element.classList.remove("enabled")
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+async function main() {
+    while (i<4){
+        var element = screen[i]
+        element.style.display = "flex";
+        await sleep(700);
+        element.style.display = "none";
+
+        if (i!==3){
+            i++;
+        } else {
+            i = 0;
+        }
+    }
+}
+
+main()
